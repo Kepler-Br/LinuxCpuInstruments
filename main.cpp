c21a8a8fd743e513ecc2183c8e1c206e890f5ab1
Barrett Kepler <45532267+Kepler-Br@users.noreply.github.com>
2019-01-31 16:40:50 +0300

    Now window and messagebox creates at center of the screen.

--- main.cpp
+++ main.cpp
@@ -3,11 +3,18 @@
 
 #include "logiccore.h"
 #include <iostream>
+#include <QDesktopWidget>
+#include <QScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    QRect screenGeometry = QGuiApplication::screens().at(0)->geometry();
+    int x = (screenGeometry.width()-w.width()) / 2;
+    int y = (screenGeometry.height()-w.height()) / 2;
+    w.move(x, y);
     w.show();
 
     return a.exec();
