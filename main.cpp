351f27d9ffd0fe1c11d63c994871963f97e16991
Barrett Kepler <kepler-br@yandex.ru>
2019-02-01 15:27:05 +0300

    Comments.

--- main.cpp
+++ main.cpp
@@ -11,10 +11,12 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 
+    // Move window to the center of the screen.
     QRect screenGeometry = QGuiApplication::screens().at(0)->geometry();
     int x = (screenGeometry.width()-w.width()) / 2;
     int y = (screenGeometry.height()-w.height()) / 2;
     w.move(x, y);
+
     w.show();
 
     return a.exec();
